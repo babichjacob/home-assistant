1307e811d87f914c9844a36efb6b2976e14465be
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-06-02 20:53:46 -0400

    support some more lock notifications

--- custom_components/custom_backend/config/packages/locks/__init__.py
+++ custom_components/custom_backend/config/packages/locks/__init__.py
@@ -24,20 +24,29 @@ from custom_components.custom_backend.const import (
 	DATA_AUDIO,
 	DATA_CAUSE,
 	DATA_CHANNEL,
+	DATA_COLOR,
 	DATA_DATETIME,
 	DATA_FULL_NAME,
+	DATA_GOOGLE_ASSISTANT,
+	DATA_GROUP,
+	DATA_IMAGE,
 	DATA_LOW_BATTERY_LEVEL,
 	DATA_MESSAGE,
 	DATA_NICKNAME,
+	DATA_PEOPLE,
 	DATA_PERSON,
 	DATA_PERSONALIZED,
 	DATA_PUSH,
+	DATA_ROOM,
 	DATA_ROOM_WITH_LIMITED_ACCESS,
 	DATA_ROOM_WITH_FREE_ACCESS,
+	DATA_SHORT_NAME,
+	DATA_SIRI,
 	DATA_TEXT,
 	DATA_THE_CURRENT_STATUS_OF_THE_DOOR,
 	DATA_TITLE,
 	DATA_USER_ID,
+	DATA_VIRTUAL_ASSISTANT,
 	DATA_ZONE,
 	DEVICE_CLASS_BATTERY,
 	DEVICE_CLASS_PROBLEM,
@@ -47,8 +56,11 @@ from custom_components.custom_backend.const import (
 	DOMAIN_RECORDER,
 	DOMAIN_SENSOR,
 	ICON_MDI_LOCK_ALERT,
+	IMAGE_LOCK_UNLOCKED_COLOR,
 	LOCK_DOOR_TO_THE_GARAGE,
 	LOCK_FRONT_DOOR,
+	NOTIFICATION_COLOR_GREEN,
+	NOTIFICATION_COLOR_YELLOW,
 	PUSH_SOUND_AUTOUNLOCK_HAPTIC,
 	ROOM_FRONT_YARD,
 	ROOM_GARAGE,
@@ -56,11 +68,12 @@ from custom_components.custom_backend.const import (
 	ROOM_KITCHEN,
 	SERVICE_LOCK,
 	SERVICE_UNLOCK,
-	SHARED_REMOTE_UNLOCKING,
+	SHARED_MEMORY_REMOTE_UNLOCKING,
 	STATE_LOCKED,
 	STATE_UNLOCKED,
 	ZONE_BAKA_S_HOUSE,
 )
+from custom_components.custom_backend.utils import slugify
 from custom_components.custom_backend.config.packages.notify import notify
 
 
@@ -68,8 +81,6 @@ _LOGGER = getLogger(__name__)
 
 
 async def get_locks(**kwds):
-	secrets = kwds["secrets"]
-
 	locks = {
 		LOCK_DOOR_TO_THE_GARAGE: {
 			DATA_NICKNAME: "Door to the Garage",
@@ -90,6 +101,7 @@ async def get_locks(**kwds):
 		lock_data.setdefault(DATA_ALARMTYPE, f"{lock_slug}_{DATA_ALARMTYPE}")
 		lock_data.setdefault(DATA_FULL_NAME, f"{lock_data[DATA_NICKNAME]} Lock")
 		lock_data.setdefault(DATA_LOW_BATTERY_LEVEL, f"{lock_slug}_{DATA_LOW_BATTERY_LEVEL}")
+		lock_data.setdefault(DATA_SHORT_NAME, "Lock")
 		lock_data.setdefault(DATA_THE_CURRENT_STATUS_OF_THE_DOOR, f"{lock_slug}_{DATA_THE_CURRENT_STATUS_OF_THE_DOOR}")
 		lock_data.setdefault(DATA_ZONE, ZONE_BAKA_S_HOUSE)
 
@@ -228,7 +240,7 @@ class PersonalizedLock(LockEntity):
 	async def async_unlock(self):
 		"""Unlock all or specified locks. A code to unlock the lock with may optionally be specified."""
 		
-		self.hass.data[DOMAIN_CUSTOM_BACKEND][SHARED_REMOTE_UNLOCKING][self._lock_slug] = {
+		self.hass.data[DOMAIN_CUSTOM_BACKEND][SHARED_MEMORY_REMOTE_UNLOCKING][self._lock_slug] = {
 			DATA_DATETIME: datetime.now(),
 			DATA_PERSON: self._person_slug,
 		}
@@ -241,6 +253,8 @@ class PersonalizedLock(LockEntity):
 				DATA_PERSON: self._person_slug,
 			},
 			DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
+			DATA_COLOR: NOTIFICATION_COLOR_GREEN,
+			DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
 			DATA_TEXT: {
 				DATA_MESSAGE: f"{self._person_data[DATA_NICKNAME]} unlocked the {self._lock_data[DATA_NICKNAME].lower()} with Home Assistant!",
 				DATA_TITLE: f"{self._lock_data[DATA_NICKNAME]} Unlocked",
@@ -263,22 +277,123 @@ class PersonalizedLock(LockEntity):
 			self._state = real_lock_state_object.state
 
 	async def async_added_to_hass(self):
+		# TODO: replace with entity state stores, perhaps derived
 		update_soon = lambda: self.async_schedule_update_ha_state(force_refresh=True)
 
 		update_soon()
 		async_track_state_change(hass=self.hass, entity_ids=[f"{DOMAIN_LOCK}.{self._lock_slug}"], action=lambda entity_id, from_state, to_state: update_soon())
 
 
+class VirtualAssistantLock(LockEntity):
+	should_poll = False
+
+	def __init__(self, *, lock_slug, lock_data, virtual_assistant, kwds):
+		self._state = None
+
+		self._lock_slug = lock_slug
+		self._lock_data = lock_data
+		self._virtual_assistant = virtual_assistant
+		self._kwds = kwds
+
+		self.entity_id = f"{DOMAIN_LOCK}.{self.unique_id}"
+	
+	@property
+	def unique_id(self):
+		"""Return Unique ID string."""
+		return f"{self._lock_slug}_{DATA_VIRTUAL_ASSISTANT}_{slugify(self._virtual_assistant)}"
+	
+	@property
+	def name(self):
+		return f"{self._lock_data[DATA_FULL_NAME]} (used through {self._virtual_assistant})"
+
+	@property
+	def available(self):
+		"""Return if the device is online."""
+		# TODO
+		return True
+
+	@property
+	def is_locked(self):
+		"""Return true if lock is locked, else False."""
+		return self._state == STATE_LOCKED
+
+	@property
+	def is_unlocked(self):
+		"""Return true if lock is unlocked, else False."""
+		return self._state == STATE_UNLOCKED
+	
+	@property
+	def state(self):
+		if self.is_locked:
+			return STATE_LOCKED
+		if self.is_unlocked:
+			return STATE_UNLOCKED
+		
+		return None
+
+	async def async_lock(self):
+		"""Lock all or specified locks. A code to lock the lock with may optionally be specified."""
+
+		await self.hass.services.async_call(DOMAIN_LOCK, SERVICE_LOCK, {
+			CONF_ENTITY_ID: f"{DOMAIN_LOCK}.{self._lock_slug}",
+		}, blocking=True)
+
+	async def async_unlock(self):
+		"""Unlock all or specified locks. A code to unlock the lock with may optionally be specified."""
+		
+		self.hass.data[DOMAIN_CUSTOM_BACKEND][SHARED_MEMORY_REMOTE_UNLOCKING][self._lock_slug] = {
+			DATA_DATETIME: datetime.now(),
+			DATA_VIRTUAL_ASSISTANT: self._virtual_assistant,
+		}
+		
+		notify_args = {
+			DATA_AUDIO: {
+				DATA_PUSH: PUSH_SOUND_AUTOUNLOCK_HAPTIC,
+			},
+			DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
+			DATA_COLOR: NOTIFICATION_COLOR_YELLOW,
+			DATA_GROUP: "Household Activity",
+			DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
+			DATA_TEXT: {
+				DATA_MESSAGE: f"The {self._lock_data[DATA_NICKNAME].lower()} was unlocked with {self._virtual_assistant}!",
+				DATA_TITLE: f"{self._lock_data[DATA_NICKNAME]} Unlocked",
+			},
+		}
+
+		notify_task = notify(notify_args, hass=self.hass, **self._kwds)
+
+		unlock_task = self.hass.services.async_call(DOMAIN_LOCK, SERVICE_UNLOCK, {
+			CONF_ENTITY_ID: f"{DOMAIN_LOCK}.{self._lock_slug}",
+		}, blocking=True)
+
+		await gather(notify_task, unlock_task)
+
+	async def async_update(self):
+		real_lock_state_object = self.hass.states.get(f"{DOMAIN_LOCK}.{self._lock_slug}")
+		if real_lock_state_object is None:
+			self._state = None
+		else:
+			self._state = real_lock_state_object.state
+
+	async def async_added_to_hass(self):
+		# TODO: replace with entity state stores, perhaps derived
+		update_soon = lambda: self.async_schedule_update_ha_state(force_refresh=True)
+
+		update_soon()
+		async_track_state_change(hass=self.hass, entity_ids=[f"{DOMAIN_LOCK}.{self._lock_slug}"], action=lambda entity_id, from_state, to_state: update_soon())
+
+
+
 async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 	hass.data.setdefault(DOMAIN_CUSTOM_BACKEND, {})
-	hass.data[DOMAIN_CUSTOM_BACKEND].setdefault(SHARED_REMOTE_UNLOCKING, {})
+	hass.data[DOMAIN_CUSTOM_BACKEND].setdefault(SHARED_MEMORY_REMOTE_UNLOCKING, {})
 
 
 async def async_setup_platform_lock(hass: core.HomeAssistant, config: dict, async_add_entities, discovery_info=None, **kwds) -> bool:
-	people = kwds["people"]
+	people = kwds[DATA_PEOPLE]
 	locks = await get_locks(**kwds)
 	
-	personalized_lock_entities = []
+	lock_entities = []
 
 	for lock_slug, lock_data in locks.items():
 		for person_slug, person_data in people.items():
@@ -286,7 +401,29 @@ async def async_setup_platform_lock(hass: core.HomeAssistant, config: dict, asyn
 				continue
 			
 			personalized_lock_entity = PersonalizedLock(lock_slug=lock_slug, lock_data=lock_data, person_slug=person_slug, person_data=person_data, kwds=kwds)
-			personalized_lock_entities.append(personalized_lock_entity)
+			lock_entities.append(personalized_lock_entity)
+		
+		for virtual_assistant in {"Google Assistant", "Siri"}:
+			virtual_assistant_lock_entity = VirtualAssistantLock(lock_slug=lock_slug, lock_data=lock_data, virtual_assistant=virtual_assistant, kwds=kwds)
+			lock_entities.append(virtual_assistant_lock_entity)
 
-	async_add_entities(personalized_lock_entities)
+	async_add_entities(lock_entities)
 	return True
+
+
+async def exposed_devices(**kwds):
+	locks = await get_locks(**kwds)
+
+	virtual_assistant_locks = {
+		f"{DOMAIN_LOCK}.{lock_slug}_{DATA_VIRTUAL_ASSISTANT}_{slugify(virtual_assistant)}": {
+			DATA_FULL_NAME: lock_data[DATA_FULL_NAME],
+			DATA_GOOGLE_ASSISTANT: virtual_assistant == "Google Assistant",
+			DATA_ROOM: lock_data[DATA_ROOM_WITH_LIMITED_ACCESS],
+			DATA_SHORT_NAME: lock_data[DATA_SHORT_NAME],
+			DATA_SIRI: virtual_assistant == "Siri",
+		} for lock_slug, lock_data in locks.items() for virtual_assistant in ["Google Assistant", "Siri"]
+	}
+
+	return {
+		**virtual_assistant_locks,
+	}
--- custom_components/custom_backend/config/packages/locks/activity.py
+++ custom_components/custom_backend/config/packages/locks/activity.py
@@ -5,9 +5,11 @@ from datetime import datetime, timedelta
 from logging import getLogger
 
 from homeassistant import core
-from homeassistant.helpers.event import async_track_state_change
 
-from custom_components.custom_backend.const import CHANNEL_ATTACKS, CHANNEL_LOCK_ACTIVITY, CONF_ENTITY_ID, CONF_OPTION, DATA_ACTION, DATA_ACTIONS, DATA_ALARMLEVEL, DATA_ALARMTYPE, DATA_AUDIO, DATA_CAMERA, DATA_CAUSE, DATA_CHANNEL, DATA_CLOSE_GARAGE_DOOR, DATA_DATETIME, DATA_FULL_NAME, DATA_GROUP, DATA_ID, DATA_IMAGE, DATA_IN_VIEW, DATA_LOCKS, DATA_LOCK_SLOTS, DATA_MESSAGE, DATA_NICKNAME, DATA_OPEN_GARAGE_DOOR, DATA_PERSON, DATA_PHOTO, DATA_PRIORITY, DATA_PUSH, DATA_REQUIRE_DEVICE_UNLOCK, DATA_ROOM, DATA_ROOM_WITH_FREE_ACCESS, DATA_ROOM_WITH_LIMITED_ACCESS, DATA_SHORT_NAME, DATA_TEXT, DATA_TITLE, DATA_ZONE, DOMAIN_COVER, DOMAIN_CUSTOM_BACKEND, DOMAIN_INPUT_SELECT, DOMAIN_SENSOR, EVENT_TYPE_HOMEASSISTANT_START, IMAGE_ACCESS_DENIED_COLOR, IMAGE_FRAUD_COLOR, IMAGE_LOCK_UNLOCKED_COLOR, IMAGE_WRONG_PINCODE_COLOR, PRIORITY_NORMAL, PRIORITY_URGENT, PUSH_SOUND_AUTOUNLOCK_HAPTIC, SERVICE_CLOSE_COVER, SERVICE_OPEN_COVER, SERVICE_SELECT_OPTION, SHARED_REMOTE_UNLOCKING
+from custom_components.custom_backend.const import (
+	CHANNEL_ATTACKS, CHANNEL_LOCK_ACTIVITY, CONF_ENTITY_ID, CONF_OPTION, DATA_ACTION, DATA_ACTIONS, DATA_ALARMLEVEL, DATA_ALARMTYPE, DATA_AUDIO, DATA_CAMERA, DATA_CAUSE, DATA_CHANNEL, DATA_CLOSE_GARAGE_DOOR, DATA_COLOR, DATA_DATETIME, DATA_FULL_NAME, DATA_GROUP, DATA_ID, DATA_IMAGE, DATA_IN_VIEW, DATA_LOCKS, DATA_LOCK_SLOTS, DATA_MESSAGE, DATA_NICKNAME, DATA_OPEN_GARAGE_DOOR, DATA_PEOPLE, DATA_PERSON, DATA_PRIORITY, DATA_PUSH, DATA_REQUIRE_DEVICE_UNLOCK, DATA_ROOM, DATA_ROOM_WITH_FREE_ACCESS, DATA_ROOM_WITH_LIMITED_ACCESS, DATA_SHORT_NAME, DATA_TEXT, DATA_TITLE, DATA_ZONE, DOMAIN_COVER, DOMAIN_CUSTOM_BACKEND, DOMAIN_INPUT_SELECT, DOMAIN_SENSOR, EVENT_TYPE_HOMEASSISTANT_START, IMAGE_ACCESS_DENIED_COLOR, IMAGE_FRAUD_COLOR, IMAGE_LOCK_UNLOCKED_COLOR, IMAGE_WRONG_PINCODE_COLOR, NOTIFICATION_COLOR_GREEN, NOTIFICATION_COLOR_ORANGERED, NOTIFICATION_COLOR_YELLOW, PRIORITY_NORMAL, PRIORITY_URGENT, PUSH_SOUND_AUTOUNLOCK_HAPTIC, SERVICE_CLOSE_COVER, SERVICE_OPEN_COVER, SERVICE_SELECT_OPTION, SHARED_MEMORY_REMOTE_UNLOCKING
+)
+from custom_components.custom_backend.utils import entity_state_to_readable_store, window
 from custom_components.custom_backend.config.packages.cameras import get_cameras
 from custom_components.custom_backend.config.packages.garage_doors import get_garage_doors
 from custom_components.custom_backend.config.packages.notify import notify
@@ -18,9 +20,8 @@ from . import get_locks
 
 _LOGGER = getLogger(__name__)
 
-
 async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
-	people = kwds["people"]
+	people = kwds[DATA_PEOPLE]
 	cameras = await get_cameras(**kwds)
 	garage_doors = await get_garage_doors(**kwds)
 	locks = await get_locks(**kwds)
@@ -39,8 +40,6 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 
 		cameras_in_the_free_access_room_that_do_not_show_the_lock = [camera_slug for camera_slug in cameras_in_the_free_access_room if camera_slug not in cameras_in_the_free_access_room_that_show_the_lock]
 		cameras_in_the_limited_access_room_that_do_not_show_the_lock = [camera_slug for camera_slug in cameras_in_the_limited_access_room if camera_slug not in cameras_in_the_limited_access_room_that_show_the_lock]
-
-		_LOGGER.warning(f"called lock_alarm_type_and_level_changed: {lock_slug} {alarm_type} {alarm_level}")
 
 		notify_args = {}
 		
@@ -93,8 +92,11 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 				notify_args[DATA_CAUSE] = {
 					DATA_PERSON: most_likely_unlocker,
 				}
+				notify_args[DATA_COLOR] = NOTIFICATION_COLOR_GREEN
+				notify_args[DATA_IMAGE] = IMAGE_LOCK_UNLOCKED_COLOR
 			else:
-				notify_args[DATA_GROUP] = f"{lock_data[DATA_FULL_NAME]} Activity"
+				notify_args[DATA_COLOR] = NOTIFICATION_COLOR_YELLOW
+				notify_args[DATA_GROUP] = "Household Activity"
 				notify_args[DATA_PRIORITY] = PRIORITY_URGENT
 				notify_args[DATA_IMAGE] = IMAGE_FRAUD_COLOR
 
@@ -109,46 +111,46 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 					} for garage_door_slug in matching_garage_doors
 				]
 			
-			if most_likely_unlocker:
+			if unlockers_in_order_of_likeliness:
 				async def set_zone_and_room():
-					zone_input_select_entity_id = f"{DOMAIN_INPUT_SELECT}.{most_likely_unlocker}_{DATA_ZONE}"
-					current_zone = hass.states.get(zone_input_select_entity_id).state
-
-					await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
-						CONF_ENTITY_ID: zone_input_select_entity_id,
-						CONF_OPTION: lock_data[DATA_ZONE],
-					}, blocking=True)
-
-					if current_zone == OPTION_UNTRACKABLE:
-						await sleep(0)
+					for person_slug in unlockers_in_order_of_likeliness:
+						zone_input_select_entity_id = f"{DOMAIN_INPUT_SELECT}.{person_slug}_{DATA_ZONE}"
+						current_zone = hass.states.get(zone_input_select_entity_id).state
 
 						await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
 							CONF_ENTITY_ID: zone_input_select_entity_id,
-							CONF_OPTION: OPTION_UNTRACKABLE,
+							CONF_OPTION: lock_data[DATA_ZONE],
 						}, blocking=True)
-					
-					room_input_select_entity_id = f"{DOMAIN_INPUT_SELECT}.{most_likely_unlocker}_{DATA_ROOM}"
-					await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
-						CONF_ENTITY_ID: room_input_select_entity_id,
-						CONF_OPTION: lock_data[DATA_ROOM_WITH_LIMITED_ACCESS],
-					}, blocking=True)
 
-					await sleep(1)
-					
-					await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
-						CONF_ENTITY_ID: room_input_select_entity_id,
-						CONF_OPTION: lock_data[DATA_ROOM_WITH_FREE_ACCESS],
-					}, blocking=True)
+						if current_zone == OPTION_UNTRACKABLE:
+							await sleep(0)
+
+							await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
+								CONF_ENTITY_ID: zone_input_select_entity_id,
+								CONF_OPTION: OPTION_UNTRACKABLE,
+							}, blocking=True)
+						
+						room_input_select_entity_id = f"{DOMAIN_INPUT_SELECT}.{person_slug}_{DATA_ROOM}"
+						await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
+							CONF_ENTITY_ID: room_input_select_entity_id,
+							CONF_OPTION: lock_data[DATA_ROOM_WITH_LIMITED_ACCESS],
+						}, blocking=True)
+
+						await sleep(1)
+						
+						await hass.services.async_call(DOMAIN_INPUT_SELECT, SERVICE_SELECT_OPTION, {
+							CONF_ENTITY_ID: room_input_select_entity_id,
+							CONF_OPTION: lock_data[DATA_ROOM_WITH_FREE_ACCESS],
+						}, blocking=True)
 
 				hass.async_create_task(set_zone_and_room())
-
 		elif alarm_type == "21":
 			if alarm_level == "1":
 				# Manually locked by someone on the inside
-				pass
+				return
 			elif alarm_level == "2":
-				# TODO: what is this?? Is it manual lock by pressing a button on the outside?
-				...
+				# Manually locked by someone on the outside (by pressing a button on the keypad)
+				return
 		elif alarm_type == "22":
 			if alarm_level == "1":
 				notify_args = {
@@ -156,7 +158,8 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 						DATA_PUSH: PUSH_SOUND_AUTOUNLOCK_HAPTIC,
 					},
 					DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
-					DATA_GROUP: f"{lock_data[DATA_FULL_NAME]} Activity",
+					DATA_COLOR: NOTIFICATION_COLOR_GREEN,
+					DATA_GROUP: "Household Activity",
 					DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
 					DATA_TEXT: {
 						DATA_MESSAGE: f"The {lock_data[DATA_NICKNAME].lower()} was unlocked from the inside!",
@@ -178,13 +181,13 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 		elif alarm_type == "24":
 			if alarm_level == "1":
 				# Locked remotely with Home Assistant
-				pass
+				return
 		elif alarm_type == "25":
 			if alarm_level == "1":
 				do_notify = True
-				REMOTE_UNLOCKING = hass.data[DOMAIN_CUSTOM_BACKEND][SHARED_REMOTE_UNLOCKING]
-				_LOGGER.warning(f"lock alarm changed: REMOTE_UNLOCKING: {REMOTE_UNLOCKING}")
-				_LOGGER.warning(f"lock alarm changed: lock_slug: {lock_slug}")
+				REMOTE_UNLOCKING = hass.data[DOMAIN_CUSTOM_BACKEND][SHARED_MEMORY_REMOTE_UNLOCKING]
+				_LOGGER.warning(f"TODO DEBUGGING: lock alarm changed: REMOTE_UNLOCKING: {REMOTE_UNLOCKING}")
+				_LOGGER.warning(f"TODO DEBUGGING: lock alarm changed: lock_slug: {lock_slug}")
 				if lock_slug in REMOTE_UNLOCKING:
 					remote_unlocking = REMOTE_UNLOCKING.pop(lock_slug)
 
@@ -192,27 +195,30 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 
 					_LOGGER.warning(f"time_since_remote_unlock: {time_since_remote_unlock}")
 
-					if time_since_remote_unlock < timedelta(seconds=5):
+					if time_since_remote_unlock < timedelta(seconds=10):
 						do_notify = False
 				
-				if do_notify:
-					notify_args = {
-						DATA_AUDIO: {
-							DATA_PUSH: PUSH_SOUND_AUTOUNLOCK_HAPTIC,
-						},
-						DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
-						DATA_GROUP: f"{lock_data[DATA_FULL_NAME]} Activity",
-						DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
-						DATA_PRIORITY: PRIORITY_URGENT,
-						DATA_TEXT: {
-							DATA_MESSAGE: f"The {lock_data[DATA_NICKNAME].lower()} was unlocked with Home Assistant by an unknown person!",
-							DATA_TITLE: f"{lock_data[DATA_NICKNAME]} Unlocked",
-						},
-					}
+				if not do_notify:
+					return
+				
+				notify_args = {
+					DATA_AUDIO: {
+						DATA_PUSH: PUSH_SOUND_AUTOUNLOCK_HAPTIC,
+					},
+					DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
+					DATA_COLOR: NOTIFICATION_COLOR_YELLOW,
+					DATA_GROUP: "Household Activity",
+					DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
+					DATA_PRIORITY: PRIORITY_URGENT,
+					DATA_TEXT: {
+						DATA_MESSAGE: f"The {lock_data[DATA_NICKNAME].lower()} was unlocked with Home Assistant by an unknown person!",
+						DATA_TITLE: f"{lock_data[DATA_NICKNAME]} Unlocked",
+					},
+				}
 		elif alarm_type == "27":
 			if alarm_level == "1":
 				# Successful auto re-lock
-				pass
+				return
 		elif alarm_type == "112":
 			# TODO: master code change
 			if alarm_level == "0":
@@ -227,7 +233,7 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 		elif alarm_type == "161":
 			notify_args = {
 				DATA_CHANNEL: CHANNEL_ATTACKS,
-				# TODO: light [10, 95]
+				DATA_COLOR: NOTIFICATION_COLOR_ORANGERED,
 				DATA_PRIORITY: PRIORITY_URGENT,
 				DATA_TEXT: {
 					DATA_TITLE: f"{lock_data[DATA_FULL_NAME]} Alarm",
@@ -262,12 +268,13 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 					# DATA_PUSH: PUSH_SOUND_AUTOUNLOCK_HAPTIC,
 				},
 				DATA_CHANNEL: CHANNEL_LOCK_ACTIVITY,
-				DATA_GROUP: f"{lock_data[DATA_FULL_NAME]} Activity",
+				# TODO: check
+				DATA_COLOR: NOTIFICATION_COLOR_ORANGERED,
 				DATA_IMAGE: IMAGE_LOCK_UNLOCKED_COLOR,
 				DATA_PRIORITY: PRIORITY_URGENT,
 				DATA_TEXT: {
-					DATA_MESSAGE: f"The {lock_data[DATA_NICKNAME].lower()} was unlocked with Home Assistant by an unknown person!",
-					DATA_TITLE: f"{lock_data[DATA_NICKNAME]} Unlocked",
+					DATA_MESSAGE: f"The {lock_data[DATA_NICKNAME].lower()} batteries are low!",
+					DATA_TITLE: f"{lock_data[DATA_NICKNAME]} Batteries",
 				},
 			}
 			# TODO: battery alert
@@ -276,10 +283,12 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 			# TODO: battery alert
 			...
 
-		# TODO: debug
-		_LOGGER.warning(notify_args)
 		if notify_args:
 			await notify(notify_args, hass=hass, **kwds)
+		else:
+			# TODO: debug
+			_LOGGER.error(f"UNACKNOWLEDGED: called lock_alarm_type_and_level_changed: {lock_slug} {alarm_type} {alarm_level}")
+
 
 	async def get_and_check_alarm_types_and_levels(*, lock_slug):
 		lock_data = locks[lock_slug]
@@ -292,9 +301,8 @@ async def async_setup(hass: core.HomeAssistant, config: dict, **kwds) -> bool:
 	async def setup_automations(event):
 		for lock_slug, lock_data in locks.items():
 			# TODO: add back f"{DOMAIN_SENSOR}.{lock_data[DATA_ALARMLEVEL]}", if needed
-			async_track_state_change(hass=hass, entity_ids=[f"{DOMAIN_SENSOR}.{lock_data[DATA_ALARMLEVEL]}"], action=lambda entity_id, from_state, to_state, *, lock_slug=lock_slug: hass.async_create_task(get_and_check_alarm_types_and_levels(lock_slug=lock_slug)))
-
+			[_get_lock_alarm_level_changes, subscribe_to_lock_alarm_level_changes] = window(entity_state_to_readable_store(f"{DOMAIN_SENSOR}.{lock_data[DATA_ALARMLEVEL]}", hass=hass), 2)
+			subscribe_to_lock_alarm_level_changes(lambda *, lock_slug=lock_slug: hass.async_create_task(get_and_check_alarm_types_and_levels(lock_slug=lock_slug)))
 
 	hass.bus.async_listen(EVENT_TYPE_HOMEASSISTANT_START, setup_automations)
-	
 	return True
